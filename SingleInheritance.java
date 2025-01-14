3edb9d6d8874c599aea757d87f5b783b0c963639
phanindra2705 <phanindranaidug@gmail.com>
2025-01-14 00:22:01 -0600

    Program for Single Inheritance

---
+++ SingleInheritance.java
@@ -0,0 +1,22 @@
+// Parent class
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+// Child class
+class Dog extends Animal {
+    void bark() {
+        System.out.println("The dog barks.");
+    }
+}
+
+public class SingleInheritance {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();  // Inherited method
+        dog.bark(); // Child class method
+    }
+}
+
