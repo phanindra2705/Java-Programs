53e18e052031c2ff7a288e76b29642bbbba3396f
phanindra2705 <phanindranaidug@gmail.com>
2025-02-27 15:16:08 -0600

    Program on Greatest Common Divisor

---
+++ GCD.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GCD {
+    public static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + findGCD(num1, num2));
+        scanner.close();
+    }
+}
